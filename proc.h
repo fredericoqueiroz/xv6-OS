e23d3b1cf9ec2914c87c1ffa9f96fb7b531cc9f8
fredericoqueiroz <fredqueirozbh@gmail.com>
2019-10-17 17:45:05 -0300

    Ajustando variavel

--- proc.h
+++ proc.h
@@ -49,7 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  uint pticks                  // Quantidade de ticks que o processo ja rodou
+  uint pticks;                 // Quantidade de ticks que o processo ja rodou
 };
 
 // Process memory is laid out contiguously, low addresses first:
